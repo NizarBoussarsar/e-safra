230956671e5effc7df0d8597d306370f2730f0c9
Raed <raed@riseup.net>
2015-05-21 20:10:41 +0100

    Fixed 2

--- e-safra-ejb/src/main/java/services/interfaces/remote/BusinessLogicServicesRemote.java
+++ e-safra-ejb/src/main/java/services/interfaces/remote/BusinessLogicServicesRemote.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package services.interfaces.remote;
 
 import java.util.List;
@@ -69,74 +68,3 @@ public interface BusinessLogicServicesRemote {
 
 	Type findTypeByStationAndLine(Station station, Line line);
 }
-=======
-package services.interfaces.remote;
-
-import java.util.List;
-import java.util.Map;
-
-import javax.ejb.Remote;
-
-import domain.Bus;
-import domain.Line;
-import domain.Passenger;
-import domain.Section;
-import domain.Station;
-import domain.Stop;
-import domain.Type;
-import domain.User;
-
-@Remote
-public interface BusinessLogicServicesRemote {
-
-	User identification(String email, String password);
-
-	Boolean changePassword(User user, String oldPassword, String newPassword,
-			String confirmationPassword);
-
-	// // At this stage, we don't need this one
-	// Boolean profileEmailAlreadyExistsWithoutFacebookId(Profile profile);
-	//
-	// // At this stage, we don't need this one
-	// Passenger accountRegisterOrLoginWithFacebook(Profile profile);
-
-	Boolean setLineStations(Line line, Map<Integer, Station> stations);
-
-	List<Station> findStationsByLineId(Integer id);
-
-	Boolean reportBusStop(Integer nbFreePlaces, Bus bus, Station station);
-
-	List<Line> findLinesByStationId(Integer id);
-
-	List<Bus> findBusesByLineId(Integer id);
-
-	Boolean setLineBuses(Line line, List<Bus> buses);
-
-	List<Bus> findAllAvailableBuses();
-
-	Integer findStationOrderByLineId(Integer idStation, Integer idLine);
-
-	Stop findLastStopByBusId(Integer idBus);
-
-	List<Bus> findComingSoonBuses(String stationName);
-
-	Bus findBusByNumber(String number);
-
-	Station findStationByName(String name);
-
-	Line findLineByName(String name);
-
-	List<Section> findSectionByStationsAndLine(Station stationSource,
-			Station stationDest, Line line);
-
-	Boolean buyTicket(Passenger passenger, Bus bus, Type typeDeparture,
-			Type typeArrival, Double price);
-
-	List<Bus> findComingSoonBusesGoingToStation(String departureStationName,
-			String arrivalStationName);
-
-	Boolean assignSectionToLine(Integer idLine,
-			Map<Section, List<Station>> stationsMap);
-
-}
->>>>>>> parent of cb0a130... 
